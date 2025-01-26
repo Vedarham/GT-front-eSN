b9bddfc8b036ca73d8e424798ea08c50de72a277
vedastra <weblog.arham129@gmail.com>
2025-01-27 02:17:26 +0530

    try commit 3

--- src/components/NavBar/Navigation.js
+++ src/components/NavBar/Navigation.js
@@ -27,19 +27,19 @@ const Navigation = () => {
       {/* this state is temporarily added onClick it will be active and on ful-screencit will automatic close */}
         <div className="nav-left">
           <a href="/" className="nav-link">Home</a>
-          <a href="/GT-front-eSN/tournaments" className="nav-link">Tournaments</a>
-          <a href="/GT-front-eSN/leaderboards" className="nav-link">Leaderboards</a>
+          <a href="/tournaments" className="nav-link">Tournaments</a>
+          <a href="/leaderboards" className="nav-link">Leaderboards</a>
           {/* <a href="/news" className="nav-link">News</a> */}
-          <a href="/GT-front-eSN/shop" className="nav-link">Shop</a>
-          <a href='/GT-front-eSN/profile' className='nav-link'>Profile</a>
+          <a href="/shop" className="nav-link">Shop</a>
+          <a href='/profile' className='nav-link'>Profile</a>
         </div>
         <div className="nav-right">
-          <a href="/GT-front-eSN/Event-Organizers" className="nav-link">Organizers</a>
-          <a href="/GT-front-eSN/subscription" className="nav-link">Subscription</a>
-          <a href="/GT-front-eSN/FAQ" className="nav-link">FAQ</a>
-          <a href="/GT-front-eSN/ContactForm" className="nav-link">Contact Us</a>
-          <a href="/GT-front-eSN/about-us" className="nav-link">About Us</a>        
-        <a href="/GT-front-eSN/login" className="nav-link login-btn">Login</a>
+          <a href="/Event-Organizers" className="nav-link">Organizers</a>
+          <a href="/subscription" className="nav-link">Subscription</a>
+          <a href="/FAQ" className="nav-link">FAQ</a>
+          <a href="/ContactForm" className="nav-link">Contact Us</a>
+          <a href="/about-us" className="nav-link">About Us</a>        
+        <a href="/login" className="nav-link login-btn">Login</a>
     
         </div>
        
